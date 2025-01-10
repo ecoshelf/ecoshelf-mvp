6b72899d5969239aa94fd56b67fb89a7a690179a
webcrawlers_br <git@webcrawlers.com.br>
2025-01-09 23:10:12 -0300

    initial commit for parameters file

---
+++ users/parameters.py
@@ -0,0 +1,5 @@
+MONGODB_USERS_HOST = 'cluster0.rn3cc.mongodb.net'
+MONGODB_USERS_USER = ''
+MONGODB_USERS_PASSWORD = ''
+MONGODB_USERS_URI = (f"mongodb+srv://{MONGODB_USERS_USER}:{MONGODB_USERS_PASSWORD}@{MONGODB_USERS_HOST}/?retryWrites="
+                     f"true&w=majority&appName=Cluster0")
