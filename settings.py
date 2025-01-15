d5fbda0dda4c445560797de7be98099c6f328165
webcrawlers_br <git@webcrawlers.com.br>
2025-01-14 22:36:22 -0300

    move file to root dir

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ MONGODB_USERS_DATABASE = 'ecoshelf'
 MONGODB_USERS_COLLECTION = 'users'
 MONGODB_USERS_HOST = 'mongodb+srv://cluster0.rn3cc.mongodb.net/ecoshelf'
 MONGODB_USERS_USER = 'ecoshelf'
-MONGODB_USERS_PASSWORD = ''
+MONGODB_USERS_PASSWORD = '57Wpvq4UfOVo'
 MONGODB_USERS_URI = 'mongodb+srv://ecoshelf:57Wpvq4UfOVo@cluster0.rn3cc.mongodb.net/ecoshelf'
 
