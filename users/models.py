711130d69574ce66b5da0c160cd0b007303c949c
webcrawlers_br <git@webcrawlers.com.br>
2025-01-14 22:31:50 -0300

    simplified users model

--- users/models.py
+++ users/models.py
@@ -1,15 +1,13 @@
+from datetime import datetime
 import mongoengine as mongo
 
-
-class UserPreferences(mongo.document):
-    ads_enabled = mongo.BooleanField(default=True)
 
 class Users(mongo.Document):
     phone_number = mongo.StringField(required=True, unique=True)
     first_name = mongo.StringField()
     last_name = mongo.StringField()
-    preferences = mongo.ListField(UserPreferences, required=True)
-    created_at = mongo.DateTimeField(required=True)
-    updated_at = mongo.DateTimeField(required=True)
-    last_seen = mongo.DateTimeField(required=True)
-    is_active = mongo.DateTimeField(required=True)
+    ads_enabled = mongo.BooleanField(default=True)
+    is_active = mongo.BooleanField(required=True)
+    updated_at = mongo.DateTimeField(required=True, default=datetime.now())
+
+
