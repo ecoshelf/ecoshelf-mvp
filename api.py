109c6dd31b2dc62bc908972eec91719f2ea5b346
LeonardoSalomao <leolrsalomao@gmail.com>
2025-01-15 22:50:52 -0300

    docker-compose and crud

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query
 from mongo_db_atlas_adaptor import MongoAdaptor
 from helpers import convert_mongo_results_to_dict
 
@@ -16,3 +16,31 @@ def get_user_by_phone_number(
 
     results = mongo.find({'phone_number': phone_number})
     return convert_mongo_results_to_dict(results)
+
+@app.post("/users")
+def create_user(user: dict):
+    try:
+        results = mongo.upsert_one(user)
+        return {"message": "Usuário criado com sucesso", "id": str(results.inserted_id)}
+    except Exception as e:
+        return {"error": str(e)}
+
+
+from bson import ObjectId
+
+@app.patch("/users/{user_id}/is_active/")
+def update_is_active(
+    user_id: str = Path(description="ID do usuário no banco de dados"),
+    is_active: bool = Query(description="Novo status de is_active")
+):
+    try:
+        user_id = ObjectId(user_id)
+        results = mongo.upsert_one(
+            {"_id": user_id},
+            {"$set": {"is_active": is_active}}
+        )
+        if results.matched_count == 0:
+            return {"message": "Usuário não encontrado"}
+        return {"message": "Status do usuário atualizado com sucesso"}
+    except Exception as e:
+        return {"error": str(e)}
