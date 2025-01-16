7d443eac0b1b5eeed348f2d7c2882abd552cc5f6
webcrawlers_br <git@webcrawlers.com.br>
2025-01-16 00:28:34 -0300

    feat: added create_user method

--- api.py
+++ api.py
@@ -1,47 +1,35 @@
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI, Path
 from mongo_db_atlas_adaptor import MongoAdaptor
 from helpers import convert_mongo_results_to_dict
-from bson import ObjectId
+from models import Users
 
 app = FastAPI()
 mongo = MongoAdaptor()
 
-@app.get("/users")
+@app.get("/users", status_code=200)
 def get_users():
     results = mongo.find_all()
     return convert_mongo_results_to_dict(results)
 
-@app.get("/users/phone_number/{phone_number}")
+@app.get("/users/phone_number/{phone_number}", status_code=200)
 def get_user_by_phone_number(
         phone_number: str = Path(description="Insira o número de whatsapp do usuário")):
 
     results = mongo.find({'phone_number': phone_number})
     return convert_mongo_results_to_dict(results)
 
-@app.post("/users")
+@app.post("/users", status_code=201)
 def create_user(user: dict):
     try:
-        results = mongo.upsert_one(user)
-        return {"message": "Usuário criado com sucesso", "id": str(results.inserted_id)}
+        user_obj = Users(phone_number=user.get('phone_number'),
+                        first_name=user.get('first_name'),
+                        last_name=user.get('last_name'),
+                        ads_enabled=user.get('ads_enabled'),
+                        is_active=user.get('is_active'),
+                        updated_at=user.get('updated_at')
+                        )
+        mongo.upsert_one(user_obj)
+        return {"message": "Usuário criado com sucesso"}
     except Exception as e:
         return {"error": str(e)}
 
-
-
-
-@app.patch("/users/{user_id}/is_active/")
-def update_is_active(
-    user_id: str = Path(description="ID do usuário no banco de dados"),
-    is_active: bool = Query(description="Novo status de is_active")
-):
-    try:
-        user_id = ObjectId(user_id)
-        results = mongo.upsert_one(
-            {"_id": user_id},
-            {"$set": {"is_active": is_active}}
-        )
-        if results.matched_count == 0:
-            return {"message": "Usuário não encontrado"}
-        return {"message": "Status do usuário atualizado com sucesso"}
-    except Exception as e:
-        return {"error": str(e)}
