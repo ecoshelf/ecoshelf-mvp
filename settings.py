713bf6691746fca6ffe48f60698d66170865badf
webcrawlers_br <git@webcrawlers.com.br>
2025-01-15 23:10:04 -0300

    fix: added settings to gitignore

--- settings.py
+++
@@ -1,7 +0,0 @@
-MONGODB_USERS_DATABASE = 'ecoshelf'
-MONGODB_USERS_COLLECTION = 'users'
-MONGODB_USERS_HOST = 'mongodb+srv://cluster0.rn3cc.mongodb.net/ecoshelf'
-MONGODB_USERS_USER = 'ecoshelf'
-MONGODB_USERS_PASSWORD = ''
-MONGODB_USERS_URI = 'mongodb+srv://ecoshelf:57Wpvq4UfOVo@cluster0.rn3cc.mongodb.net/ecoshelf'
-
