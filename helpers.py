6e96bae60a7fee969d69d77ca048a4593a51ef9b
webcrawlers_br <git@webcrawlers.com.br>
2025-01-16 22:24:10 -0300

    test: converts bson id to json

--- helpers.py
+++ helpers.py
@@ -1,7 +1,10 @@
+from bson import json_util
+import json
+
 def convert_mongo_results_to_dict(results):
     results_list = []
     if results:
         for result in results:
-            results_list.append(result)
+            results_list.append(json.loads(json_util.dumps(result)))
         return results_list
     return None
